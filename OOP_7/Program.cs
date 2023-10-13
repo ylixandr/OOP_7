14889451d248e387e051031e32b1174ff1fb6b8f
Ylixandr <savoshinsky55@gmail.com>
2023-10-13 18:54:35 +0300

    Добавьте файлы проекта.

---
+++ OOP_7/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Win32.SafeHandles;
+using OOP_7;
+using System.Drawing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rect rect = new Rect(2, 4);
+        rect.GetPerimeter();
+        rect.GetSquare();
+
+    }
+}
---
+++ OOP_7/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_7
+{
+    public class Rect
+    {
+
+       private int width, height;
+
+        public Rect(int width, int heigth)
+        {
+           this.width = width;
+            this.height = heigth;
+        }
+
+
+        //public int X { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        //public int Y { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        
+
+        public void GetPerimeter()
+        {
+            Console.WriteLine($"Perimeter is: {height*2+width*2}");
+        }
+
+        public void GetSquare()
+        {
+            Console.WriteLine($"Square = {width*height}");
+
+        }
+    }
+}
